c83080c60f7ce78bf2594c1102258f4395134f9a
Miguel Oseguera <ml.oseguera@yahoo.com>
2025-09-20 14:42:01 -0500

    chore: save local changes before merge

--- src/app/events/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function EventsPage() {
-  return (
-    <div className="mx-auto max-w-6xl px-4 py-8 text-white">
-      <h1 className="sigmar-regular text-3xl mb-4">Events</h1>
-      <p>Content coming soon.</p>
-    </div>
-  );
-}
